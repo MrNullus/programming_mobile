f8f822aa902b32e886b4799a67f002b675ca10ed
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-08-17 18:44:09 -0300

    init repo

---
+++ IntroducaoJavaScript/js/imc.js
@@ -0,0 +1,28 @@
+$(document).ready(function () { 
+
+	$('#btnCalcularIMC').click(function () {
+		
+		// pegar dados
+		let peso = parseFloat($('#txtPeso').val());
+		let altura = parseFloat($('#txtAltura').val());
+		
+		// realizar calculo do IMC
+		let imc = (peso / (altura ** 2)).toFixed(2);
+		
+		// verificar IMC
+		if () {
+			
+		} else if () {
+			
+		} else if () {
+			
+		} else if () {
+			
+		}
+		
+		// exibir resultado
+		
+		
+	});
+
+});	
